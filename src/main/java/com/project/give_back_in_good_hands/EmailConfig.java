c52beb01e2e7e2998b2196f475333b2d71008ed8
sebastianpalarczyk <s.palarczyk12@gmail.com>
2021-02-18 22:09:01 +0100

    addedSendingRegisterEmail

--- src/main/java/com/project/give_back_in_good_hands/EmailConfig.java
+++
@@ -1,9 +0,0 @@
-package com.project.give_back_in_good_hands;
-
-
-import org.springframework.context.annotation.PropertySource;
-
-
-@PropertySource(value = "classpath:application.properties")
-public class EmailConfig {
-}
--- src/main/java/com/project/give_back_in_good_hands/web/RegisterController.java
+++ src/main/java/com/project/give_back_in_good_hands/web/RegisterController.java
@@ -1,7 +1,7 @@
 package com.project.give_back_in_good_hands.web;
 
 import com.project.give_back_in_good_hands.domain.User;
-import com.project.give_back_in_good_hands.repository.RoleRepository;
+import com.project.give_back_in_good_hands.service.EmailService;
 import com.project.give_back_in_good_hands.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,11 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RegisterController {
 
     private final UserService userService;
-    private final RoleRepository roleRepository;
+    private final EmailService emailService;
 
-    public RegisterController(UserService userService, RoleRepository roleRepository) {
+    public RegisterController(UserService userService, EmailService emailService) {
         this.userService = userService;
-        this.roleRepository = roleRepository;
+        this.emailService = emailService;
     }
 
     @RequestMapping(value = "/register")
@@ -29,6 +29,14 @@ public class RegisterController {
     @PostMapping(value = "/register")
     public String create(@ModelAttribute User user){
         userService.saveUser(user);
+        emailService.sendMessageWithAttachment(user.getEmail(),
+                "Rejestracja w aplikacji odddam w dobre ręce",
+                "Witamy, "+user.getFirstName()+"\n"+"\n"
+                        +"fajnie, że do nas dołączyłeś!\n"
+                        +"\n"
+                        +"Pozdrawiamy,\n"
+                        +"Zespół Oddaj w dobre ręce",
+                "/home/sebastian/Pulpit/Regulamin.pdf");
         return "redirect:login";
     }
 }
