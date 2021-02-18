574ab2e446da68a7ddd8cec4f224d85441378374
sebastianpalarczyk <s.palarczyk12@gmail.com>
2021-02-18 18:59:35 +0100

    addedProject

---
+++ src/main/java/com/project/give_back_in_good_hands/GiveBackInGoodHandsApplication.java
@@ -0,0 +1,13 @@
+package com.project.give_back_in_good_hands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiveBackInGoodHandsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiveBackInGoodHandsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/give_back_in_good_hands/GiveBackInGoodHandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.give_back_in_good_hands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiveBackInGoodHandsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
